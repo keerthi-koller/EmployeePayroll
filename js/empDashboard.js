f71db0f3def30a166748f8f439b751b2d7c07b53
keerthi-koller <keerthi.nk8@gmail.com>
2023-12-27 17:03:41 +0530

    Completed Employee Pay roll Website

--- js/empDashboard.js
+++ js/empDashboard.js
@@ -7,20 +7,42 @@ window.addEventListener("DOMContentLoaded",function(){
       location.href = "../pages/emppayrollform.html"
     })
 
+    const searchBtn = this.document.getElementById("searchBtn");
+    searchBtn.addEventListener("click", function () {
+      let searchId = document.getElementById("searchId").value;
+      searchFun(searchId);
+    })
+
 })
 
-function deleteFun (tr1) {
-   const tr2 = document.getElementById(`${tr1}`);
-   console.log(tr2);
+function editFun (obj) {
+   let dataLoc = JSON.parse(localStorage.getItem("EmployeeDetails"));
+   let d = dataLoc.filter(x => x._name == obj);
+
+   localStorage.setItem("editEmployeeDetails", JSON.stringify(d[0]));
+
+   location.href = "../pages/emppayrollform.html";
+
 }
 
-function getEmployeeData() {
-   const data = JSON.parse(localStorage.getItem("EmployeeDetails"));
+function deleteFun (obj) {
+   let dataLoc = JSON.parse(localStorage.getItem("EmployeeDetails"));
+   let d = parseInt(dataLoc.findIndex(x => x._name === obj))
+   let f = dataLoc.splice(d, 1);
+
+   localStorage.setItem("EmployeeDetails", JSON.stringify(dataLoc));
+
+   getEmployeeData();
+}
+
+function searchFun(search) {
+   let searchEle = JSON.parse(localStorage.getItem("EmployeeDetails"));
+   let searchEle1 = searchEle.filter(x => x._name == search);
+   
    const tbody = document.getElementById("tbody");
 
-   let tr1 = 0;
    let template = "";
-   for (let ele of data) {
+   for (let ele of searchEle1) {
 
       let template1 = "";
       for (let ele1 of ele._department) {
@@ -29,9 +51,8 @@ function getEmployeeData() {
          `
       }
       
-      tr1++;
       template = template + `
-                     <tr id="${tr1}">
+                     <tr>
                         <td>
                            <img src="${ele._profile}" alt="img">
                         </td>
@@ -46,18 +67,83 @@ function getEmployeeData() {
                         <td>${ele._startDate}</td>
                         <td>
                            <div class="actionsdiv">
-                                 <i class='bx bxs-trash-alt' id="delete" onclick="del()"></i>
-                                 <i class='bx bxs-pencil' id="edit"></i>
+                                 <i class='bx bxs-trash-alt' id="delete" onclick="deleteFun('${ele._name}')"></i>
+                                 <i class='bx bxs-pencil' id="edit" onclick="editFun('${ele._name}')"></i>
                            </div>
                         </td>
                      </tr>
                      `
    }
 
-   function del () {
-      console.log("Deleted");
-   }
+   tbody.innerHTML = template;
 
+   let searchDiv = document.getElementById("searchdiv");
+   searchDiv.style.position = "relative";
+   searchDiv.style.zIndex = "3";
+   let searchBtn = document.getElementById("searchBtn");
+   searchDiv.removeChild(searchBtn);
+   let cross = document.createElement("h4");
+   cross.style.position = "absolute";
+   cross.style.fontSize = "20px"
+   cross.style.left = "30px";
+   cross.style.zIndex = "21";
+   cross.innerText = "x";
+   cross.id = "crossId";
+   searchDiv.appendChild(cross);
+   crossId = document.getElementById("crossId");
+   crossId.addEventListener("click", function () {
+      getEmployeeData();
+      let h4 = document.getElementById("crossId");
+      searchDiv.removeChild(h4);
+      let searchBtn = document.createElement("i");
+      searchBtn.setAttribute("class", "bx bx-search-alt-2");
+      searchBtn.id = "searchBtn";
+      searchDiv.appendChild(searchBtn);
+      searchBtn.style.position = "absolute";
+      searchBtn.style.top = "11px";
+      searchBtn.style.right = "20px";
+      // searchBtn.style.zIndex = "233";
+      document.getElementById("searchId").value = "";
+   })
+}
+
+function getEmployeeData() {
+   const data = JSON.parse(localStorage.getItem("EmployeeDetails"));
+   const tbody = document.getElementById("tbody");
+
+   let template = "";
+   for (let ele of data) {
+
+      let template1 = "";
+      for (let ele1 of ele._department) {
+         template1 = template1 + `
+                           <div>${ele1}</div>
+         `
+      }
+      
+      template = template + `
+                     <tr>
+                        <td>
+                           <img src="${ele._profile}" alt="img">
+                        </td>
+                        <td>${ele._name}</td>
+                        <td>${ele._gender}</td>
+                        <td>
+                           <div class="depttable">
+                           ${template1}
+                           </div>
+                        </td>
+                        <td>Rs. ${ele._salary},000</td>
+                        <td>${ele._startDate}</td>
+                        <td>
+                           <div class="actionsdiv">
+                                 <i class='bx bxs-trash-alt' id="delete" onclick="deleteFun('${ele._name}')"></i>
+                                 <i class='bx bxs-pencil' id="edit" onclick="editFun('${ele._name}')"></i>
+                           </div>
+                        </td>
+                     </tr>
+                     `
+   }
 
    tbody.innerHTML = template;
 }
--- js/employeeevnt.js
+++ js/employeeevnt.js
@@ -1,32 +1,86 @@
 import EmployeeDetails from "./EmployeeDetails.js";
 
-window.addEventListener("DOMContentLoaded", function (){
+window.addEventListener("DOMContentLoaded", function () {
 
     const cancel = this.document.getElementById("cancelBtn");
-    cancel.addEventListener("click", function (){
+    cancel.addEventListener("click", function () {
         window.location.href = "../pages/empdetails.html"
     })
 
     const reset = this.document.getElementById("resetBtn");
-    reset.addEventListener("click", function (){
+    reset.addEventListener("click", function () {
         resetForm();
     })
 
     const submitbtn = this.document.getElementById("submit");
-    submitbtn.addEventListener("click",function(){
+    submitbtn.addEventListener("click",function() {
         submitForm();
     })
+
+
+    if (this.localStorage.getItem("editEmployeeDetails") != null) {
+
+        let edited = JSON.parse(this.localStorage.getItem("editEmployeeDetails"));
+
+        document.getElementById("nameid").value = edited._name;
+        
+        let profile1 = this.document.getElementsByName("profile");
+        for (let ele of profile1) {
+            if (ele.value == edited._profile) {
+                ele.checked = true;
+            }
+        }
+
+        let gender1 = document.getElementsByName("gender")
+        for (let ele of gender1) {
+            if (ele.value == edited._gender) {
+                ele.checked = true;
+            }
+        }
+
+        let department1 = this.document.getElementsByName("department");
+        for (let ele of department1) {
+            for (let ele1 of edited._department) {
+                if (ele.value == ele1) {
+                    ele.checked = true;
+                }
+            }
+        }
+
+        document.getElementById("salaryId").value = edited._salary;
+        document.getElementById("dateId").value = edited._startDate.substring(0, 2);
+        document.getElementById("monthId").value = edited._startDate.substring(3, 6);
+        document.getElementById("yearId").value = edited._startDate.substring(7, 11);
+
+    }
 
 })
 
 function resetForm (){
-    const input = document.getElementById("nameid");
-    const note = document.getElementById("noteid");
-    input.value = "";
-    note.value = "";
+    document.getElementById("nameid").value = "";
+    document.getElementById("noteid").value = "";
+    document.getElementById("salaryId").value = "";
+    document.getElementById("dateId").value = "Day";
+    document.getElementById("monthId").value = "Month";
+    document.getElementById("yearId").value = "Year";
+    let profile1 = document.getElementsByName("profile");
+    for (let ele of profile1) {
+        ele.checked = false;
+    }
+
+    let gender1 = document.getElementsByName("gender")
+    for (let ele of gender1) {
+        ele.checked = false;
+    }
+
+    let department1 = document.getElementsByName("department");
+    for (let ele of department1) {
+        ele.checked = false;
+    }
 }
 
 function submitForm(){
+
     const names = document.getElementById("nameid").value;
     const profile = document.querySelector('input[name="profile"]:checked').value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
@@ -44,7 +98,7 @@ function submitForm(){
     const year = document.getElementById("yearId").value;
     const startDate = `${date} ${month} ${year}`;
     const notes = document.getElementById("noteid").value;
-    
+
     let empDetails = new EmployeeDetails ();
     empDetails.setName = names;
     empDetails.setProfile = profile;
@@ -54,17 +108,28 @@ function submitForm(){
     empDetails.setStartDate = startDate;
     empDetails.setNotes = notes;
 
-    if (localStorage.getItem("EmployeeDetails") == null) {
-        localStorage.setItem("EmployeeDetails", "[]");
-    }
-
-    let data = JSON.parse(localStorage.getItem("EmployeeDetails"));
-    data.push(empDetails);
-    localStorage.setItem("EmployeeDetails", JSON.stringify(data));
-
-    console.log(JSON.parse(localStorage.getItem("EmployeeDetails")));
-
     
+    if (localStorage.getItem("editEmployeeDetails") != null) {
+        let edited = JSON.parse(localStorage.getItem("editEmployeeDetails"));
+
+        let oldData = JSON.parse(localStorage.getItem("EmployeeDetails"));
+        let index = oldData.findIndex(x => x._name == edited._name);
+
+        oldData[index] = empDetails;
+        localStorage.setItem("EmployeeDetails", JSON.stringify(oldData));
+    
+        localStorage.removeItem("editEmployeeDetails");
+    }
+    else {
+        if (localStorage.getItem("EmployeeDetails") == null) {
+            localStorage.setItem("EmployeeDetails", "[]");
+        }
+
+        let data = JSON.parse(localStorage.getItem("EmployeeDetails"));
+        data.push(empDetails);
+        localStorage.setItem("EmployeeDetails", JSON.stringify(data));
+
+    }
     location.href = "../pages/empdetails.html";
 
 }
